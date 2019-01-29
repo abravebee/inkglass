d389b6e580983a1b5d8b25fce2ca76c32ec711da
Casey Baker <caseye.baker@gmail.com>
2019-01-29 15:46:16 -0800

    implements test route

--- back-end/journalapi/controllers/entries.js
+++ back-end/journalapi/controllers/entries.js
@@ -1,3 +1,9 @@
 //Imports
 var ObjectId = require('mongodb').ObjectID;
 const Entries = require('../models/entries');
+
+//Test Route
+exports.test = function (req, res) {
+  res.send('The prophecy is true.');
+  console.log('Boom.');
+}
