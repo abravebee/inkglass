2f6a9c47b00834b578e211ff24510977df6348b2
Casey Baker <caseye.baker@gmail.com>
2019-01-29 14:36:56 -0800

    adds title to entry schema

--- back-end/journalapi/models/entries.js
+++ back-end/journalapi/models/entries.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 //Schema Definition
 let EntrySchema = new Schema({
   date: {type: Number, required: true},
+  title: {type: String, required: false},
   mood: {type: String, required: true},
   lunar: {type: String, required: false},
   horo: {type: String, required: false},
