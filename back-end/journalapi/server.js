01c30aef9e9719de299f171f7db4b79683b2e047
Casey Baker <caseye.baker@gmail.com>
2019-01-29 14:26:21 -0800

    database connection to mLab

--- back-end/journalapi/server.js
+++ back-end/journalapi/server.js
@@ -10,3 +10,9 @@ const app = express();
 //Connecting app to remote database
 //Set up mongoose connection
 const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://testuser:test123@ds245082.mlab.com:45082/entries';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
