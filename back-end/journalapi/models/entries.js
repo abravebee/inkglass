c71a02ce5ffbec72ce395e74ff59886c17076498
Casey Baker <caseye.baker@gmail.com>
2019-01-29 14:36:33 -0800

    defines entry schema

--- back-end/journalapi/models/entries.js
+++ back-end/journalapi/models/entries.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+//Schema Definition
+let EntrySchema = new Schema({
+  date: {type: Number, required: true},
+  mood: {type: String, required: true},
+  lunar: {type: String, required: false},
+  horo: {type: String, required: false},
+  tarot: {type: String, required: false},
+  ritual: {type: String, required: false},
+  notes: {type: String, required: false},
+})
