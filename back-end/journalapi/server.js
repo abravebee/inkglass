0073104504a5df5029880b5764f3d3eb9579fb0c
Casey Baker <caseye.baker@gmail.com>
2019-01-29 14:21:13 -0800

    require mongoose

--- back-end/journalapi/server.js
+++ back-end/journalapi/server.js
@@ -6,3 +6,7 @@ const journal = require('./routes/product');
 
 //Initialize express app
 const app = express();
+
+//Connecting app to remote database
+//Set up mongoose connection
+const mongoose = require('mongoose');
