25a759b1507877e8f942626c0ff29d2ab54940f2
Casey Baker <caseye.baker@gmail.com>
2019-01-29 16:45:34 -0800

    adds controller for entries create

--- back-end/journalapi/controllers/entries.js
+++ back-end/journalapi/controllers/entries.js
@@ -1,10 +1,30 @@
 //Imports
 var ObjectId = require('mongodb').ObjectID;
-const Entries = require('../models/entries');
+const Entry = require('../models/entries');
 
 //Test Route
 exports.test = function (req, res) {
-  res.send('<h1>The prophecy is true.</h1>');
+  res.send('The prophecy is true.');
   console.log('Boom.');
 }
 
+//CRUD
+//Create
+exports.entryCreate = (req, res) => {
+  let entry = new Entry(
+    {
+      date: req.body.date,
+      title: req.body.title,
+      mood: req.body.mood,
+      lunar: req.body.lunar,
+      horo: req.body.horo,
+      tarot: req.body.tarot,
+      notes: req.body.notes
+    }
+  );
+
+  entry.save((err) => {
+    if(err) return next(err);
+    res.send('Entry created successfully\n', entry)
+  });
+};
