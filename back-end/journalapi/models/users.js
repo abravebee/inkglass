6c7a37943905adeb7539e09bafaab60121cfe110
Casey Baker <caseye.baker@gmail.com>
2019-02-05 18:53:32 -0800

    moves entry schema definition to users.js for embed testing

--- back-end/journalapi/models/users.js
+++ back-end/journalapi/models/users.js
@@ -1,16 +1,27 @@
 //Imports
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Entry = require('../models/entries');
 
 //Schema Definition
+let EntrySchema = new Schema({
+  author: {type: Schema.ObjectId},
+  date: {type: Number, required: true},
+  title: {type: String, required: false},
+  mood: {type: String, required: true},
+  lunar: {type: String, required: false},
+  horo: {type: String, required: false},
+  tarot: {type: Number, required: false},
+  notes: {type: String, required: false},
+})
+
+
 let UserSchema = new Schema({
   username: {type: String, required: true},
   name: {type: String, required: false},
   email: {type: String, required: false},
   birthday: {type: Number, required: false},
   astro: {type: String, required: false},
-  entries: [Entry]
+  entries: [EntrySchema]
 })
 
 //Export
