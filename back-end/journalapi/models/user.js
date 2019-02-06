6526d6e3d076ddf1eaa861a90261cb9adffa7330
Casey Baker <caseye.baker@gmail.com>
2019-02-05 18:23:32 -0800

    initializes model for users

---
+++ back-end/journalapi/models/user.js
@@ -0,0 +1,3 @@
+//Imports
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
--- back-end/journalapi/routes/users.js
+++ back-end/journalapi/routes/users.js
@@ -1,4 +1,4 @@
 //Imports
 const express = require('express');
 const router = express.Router();
-//const usersController = require('../controllers/users');
+const usersController = require('../controllers/users');
