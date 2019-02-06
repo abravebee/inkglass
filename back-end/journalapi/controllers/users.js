0a39c66ca26e9719df8f64c39fb305eb0705e90e
Casey Baker <caseye.baker@gmail.com>
2019-02-05 18:50:29 -0800

    test user controller

--- back-end/journalapi/controllers/users.js
+++ back-end/journalapi/controllers/users.js
@@ -1,3 +1,9 @@
 //Imports
 var ObjectId = require('mongodb').ObjectID;
 const User = require('../models/users');
+
+//Test Route
+exports.test = function (req, res) {
+  res.send('Users are connected.');
+  console.log('Reached user test function!')
+}
