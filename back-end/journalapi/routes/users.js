6758b95804ac2cf0ff5f402cbd2e0a3bc8da71cc
Casey Baker <caseye.baker@gmail.com>
2019-02-06 09:57:52 -0800

    create user route

--- back-end/journalapi/routes/users.js
+++ back-end/journalapi/routes/users.js
@@ -6,4 +6,8 @@ const usersController = require('../controllers/users');
 //Test Route
 router.get('/test', usersController.test);
 
+//CRUD
+//Create User
+router.post('/create', usersController.createUser);
+
 module.exports = router;
