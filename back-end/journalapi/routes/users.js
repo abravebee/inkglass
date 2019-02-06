775d65fc4c2d17338e0fbe6faa02d0caa2b9a7a1
Casey Baker <caseye.baker@gmail.com>
2019-02-05 18:51:23 -0800

    test user route

--- back-end/journalapi/routes/users.js
+++ back-end/journalapi/routes/users.js
@@ -2,3 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
+
+//Test Route
+router.get('/test', usersController.test);
