03616c7581de72d988df9f86ae36c7443e7e95ed
Casey Baker <caseye.baker@gmail.com>
2019-02-14 11:00:30 -0800

    builds entry model according to Lambda syntax example

--- back-end/journalapi/models/entries.js
+++ back-end/journalapi/models/entries.js
@@ -1,2 +1,13 @@
 //== Imports ==//
 const mongoose = require("mongoose");
+
+const EntrySchema = new mongoose.Schema({
+  _parent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
