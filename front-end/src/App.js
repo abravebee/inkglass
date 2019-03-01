3f7eca34d3e3cbb5a247e25c0d32cca8fc6d4c68
Casey Baker <caseye.baker@gmail.com>
2019-03-01 15:59:55 -0800

    pseudocoding component structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      Testing for deployment.
+      {
+        //Landing Page or Main App
+      }
       </div>
     );
   }
---
+++ front-end/src/components/innerApp/content/journal/EntryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class EntryCard extends Component {
+  render() {
+    return (
+      <div className="EntryCard">
+      {
+        //Details to show on the card.
+      }
+      </div>
+    );
+  }
+}
+
+export default EntryCard;
