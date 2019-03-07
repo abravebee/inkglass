eb4d6970e9ef7c377a51d19536e9134812bc98b4
Casey Baker <caseye.baker@gmail.com>
2019-03-06 16:25:18 -0800

    adds Router and sets up Landing route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 //== Dependencies ==//
 import React, { Component } from "react";
+import { Route, withRouter} from "react-router-dom";
 
 //== Components ==//
 import Landing from "./components/landingPage/Landing";
@@ -11,7 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      Under construction.
+      <Route path="/"
+        component={Landing}></Route>
         {
           //Landing Page or Main App
         }
@@ -20,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
