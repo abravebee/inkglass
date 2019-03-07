67ac65d831e358af24672a1b1e407763217ca9c7
Casey Baker <caseye.baker@gmail.com>
2019-03-07 15:34:44 -0800

    action type for single user

--- front-end/src/actions/userActions.js
+++ front-end/src/actions/userActions.js
@@ -1,19 +1,7 @@
 //== Dependencies ==//
 import axios from "axios";
 
-//== Dispatch Types ==//
-export const ERROR = "ERROR";
-export const FETCHING_ALL_USERS = "FETCHING_ALL_USERS";
-export const FETCHED_ALL_USERS = "FETCHED_ALL_USERS";
-export const FETCHING_USER = "FETCHING_USER";
-export const FETCHED_USER = "FETCHED_USER";
-export const ADDING_USER = "ADDING_USER";
-export const ADDED_USER = "ADDED_USER";
-export const UPDATING_USER = "UPDATING_USER";
-export const UPDATED_USER = "UPDATED_USER";
-export const DELETING_USER = "DELETING_USER";
-export const DELETED_USER = "DELETED_USER";
-
+//== Variables ==//
 const URL = "https://inkglass.now.sh/journalapi";
 
 //== Actions ==//
@@ -28,3 +16,28 @@ export const fetchAllUsers = dispatch => {
       dispatch({ type: ERROR, payload: err })
     })
 }
+
+export const fetchSingleUser = id => dispatch => {
+  dispatch({ type: FETCHING_USER });
+  axios.get(`${URL}/${id}`)
+    .then(res => {
+      //res.data = new user
+      dispatch({ type: FETCHED_USER, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err })
+    })
+}
+
+//== Dispatch Types ==//
+export const ERROR = "ERROR";
+export const FETCHING_ALL_USERS = "FETCHING_ALL_USERS";
+export const FETCHED_ALL_USERS = "FETCHED_ALL_USERS";
+export const FETCHING_USER = "FETCHING_USER";
+export const FETCHED_USER = "FETCHED_USER";
+export const ADDING_USER = "ADDING_USER";
+export const ADDED_USER = "ADDED_USER";
+export const UPDATING_USER = "UPDATING_USER";
+export const UPDATED_USER = "UPDATED_USER";
+export const DELETING_USER = "DELETING_USER";
+export const DELETED_USER = "DELETED_USER";
