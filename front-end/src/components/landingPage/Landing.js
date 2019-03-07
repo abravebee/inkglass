30a3f6dc207f83ed519821ea1fb3ed02e0832232
Casey Baker <caseye.baker@gmail.com>
2019-03-06 16:26:36 -0800

    builds Navbar with sign in sign out

--- front-end/src/components/landingPage/Landing.js
+++ front-end/src/components/landingPage/Landing.js
@@ -1,9 +1,15 @@
+//== Dependencies ==//
 import React, { Component } from "react";
+
+//== Components ==//
+import LandingNav from './LandingNav';
 
 class Landing extends Component {
   render() {
     return (
       <div className="Landing">
+      <LandingNav />
+       Under construction.
         {
           //LandingNav
           //Call to Action
---
+++ front-end/src/components/landingPage/LandingNav.js
@@ -0,0 +1,35 @@
+//== Dependencies ==//
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+//== Components ==//
+import auth0Client from '../../Auth';
+
+function LandingNav(props) {
+  const signOut = () => {
+    auth0Client.signOut();
+    props.history.replace('/');
+  };
+
+  return (
+    <nav className="LandingNav">
+      <Link to="/">
+        Inkglass
+      </Link>
+      {
+        !auth0Client.isAuthenticated() &&
+        <button onClick={auth0Client.signIn}>Sign In</button>
+      }
+      {
+        auth0Client.isAuthenticated() &&
+        <div>
+          <label>Blessed be, {auth0Client.getProfile().name}</label>
+          <button onClick={() => {signOut()}}>Sign Out</button>
+        </div>
+      }
+
+    </nav>
+  )
+}
+
+export default LandingNav;
