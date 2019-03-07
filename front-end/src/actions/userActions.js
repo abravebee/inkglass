0612375cbc1fd2c2df69505c6eafe671beb57e81
Casey Baker <caseye.baker@gmail.com>
2019-03-07 14:54:47 -0800

    adds action exports for userActions

---
+++ front-end/src/actions/userActions.js
@@ -0,0 +1,16 @@
+//== Dependencies ==//
+import axios from "axios";
+
+//== Action Exports ==//
+export const ERROR = "ERROR";
+export const FETCHING_ALL_USERS = "FETCHING_ALL_USERS";
+export const FETCHED_ALL_USERS = "FETCHED_ALL_USERS";
+export const FETCHING_USER = "FETCHING_USER";
+export const FETCHED_USER = "FETCHED_USER";
+export const ADDING_USER = "ADDING_USER";
+export const ADDED_USER = "ADDED_USER";
+export const UPDATING_USER = "UPDATING_USER";
+export const UPDATED_USER = "UPDATED_USER";
+export const DELETING_USER = "DELETING_USER";
+export const DELETED_USER = "DELETED_USER";
+
