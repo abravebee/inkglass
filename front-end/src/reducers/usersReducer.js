fd27e60a6ec9570fe312795aba0f632e23f7a473
Casey Baker <caseye.baker@gmail.com>
2019-03-07 16:30:02 -0800

    adds single user responses to end of allUsers array

--- front-end/src/reducers/usersReducer.js
+++ front-end/src/reducers/usersReducer.js
@@ -24,13 +24,13 @@ export const usersReducer = (state = initialState, action) => {
     case actions.FETCHED_ALL_USERS:
       return { ...state, fetchingAllUsers: false, allUsers: action.payload }
     case actions.FETCHING_USER:
-      return { ...state, fetchingUser: true }
+      return { ...state, fetchingUser: true };
     case actions.FETCHED_USER:
-      return { ...state, fetchingUser: false, user: action.payload, users: { ...users, action.payload } }
+      return { ...state, fetchingUser: false, user: action.payload, allUsers: [ ...allUsers, action.payload ] };
     case actions.ADDING_USER:
       return { ...state, addingUser: true };
     case actions.ADDED_USER:
-      return { ...state, addingUser: false, user: action.payload, users: { ...users, action.payload } };
+      return { ...state, addingUser: false, user: action.payload, allUsers: [ ...allUsers, action.payload ] };
     case actions.ERROR:
       return { ...state, error: action.payload }
     default:
