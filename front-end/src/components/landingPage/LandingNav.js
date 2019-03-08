b820411b261a24f4fb66db71f44be9e99f53b891
Casey Baker <caseye.baker@gmail.com>
2019-03-07 17:07:50 -0800

    adds material ui icons

--- front-end/src/components/landingPage/LandingNav.js
+++ front-end/src/components/landingPage/LandingNav.js
@@ -7,38 +7,60 @@ import auth0Client from '../../Auth';
 
 //== Styles ==//
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from `@material-ui/core/AppBar`;
-import Toolbar from `@material-ui/core/Toolbar`;
-import Typography from `@material-ui/core/Typography`;
-import Button from `@material-ui/core/Button`;
-import IconButton from `@material-ui/core/IconButton`;
-import MenuIcon from `@material-ui/core/Menu`;
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+}
 
 function LandingNav(props) {
+  const { classes } = props;
   const signOut = () => {
     auth0Client.signOut();
     props.history.replace('/');
   };
 
   return (
-    <nav className="LandingNav">
-      <Link to="/">
-        Inkglass
-      </Link>
-      {
-        !auth0Client.isAuthenticated() &&
-        <button onClick={auth0Client.signIn}>Sign In</button>
-      }
-      {
-        auth0Client.isAuthenticated() &&
-        <div>
-          <label>{auth0Client.getProfile().name}</label>
-          <button onClick={() => {signOut()}}>Sign Out</button>
-        </div>
-      }
-
+    <nav className="{classes.root}">
+      <AppBar position="static">
+        <Toolbar>
+        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Link to="/">
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+            Inkglass
+          </Typography>
+          </Link>
+        {
+          !auth0Client.isAuthenticated() &&
+          <Button color="inherit" onClick={auth0Client.signIn}>Sign In</Button>
+        }
+        {
+          auth0Client.isAuthenticated() &&
+          <div>
+            <label>{auth0Client.getProfile().name}</label>
+            <Button color="inherit" onClick={() => {signOut()}}>Sign Out</Button>
+          </div>
+        }
+        </Toolbar>
+      </AppBar>
     </nav>
   )
 }
 
-export default withRouter(LandingNav);
+export default withRouter(withStyles(styles)(LandingNav));
