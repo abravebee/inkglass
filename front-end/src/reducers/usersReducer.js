62eec5d8f43e2731e5ab6cd0745ef7cd9ca134a5
Casey Baker <caseye.baker@gmail.com>
2019-03-07 16:23:36 -0800

    reducers for fetching and adding

--- front-end/src/reducers/usersReducer.js
+++ front-end/src/reducers/usersReducer.js
@@ -15,3 +15,21 @@ let initialState = {
   updatingUser: false,
   error: null
 };
+
+//== Reducer ==//
+export const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.FETCHING_ALL_USERS:
+      return { ...state, fetchingAllUsers: true};
+    case actions.FETCHED_ALL_USERS:
+      return { ...state, fetchingAllUsers: false, allUsers: action.payload }
+    case actions.ADDING_USER:
+      return { ...state, addingUser: true };
+    case actions.ADDED_USER:
+      return { ...state, addingUser: false, user: action.payload, users: { ...users, action.payload } };
+    case actions.ERROR:
+      return { ...state, error: action.payload }
+    default:
+      return state;
+  }
+}
