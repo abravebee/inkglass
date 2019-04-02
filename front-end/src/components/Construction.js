0bef454d89238a915739218854c5d5b4b5234997
Casey Bee <caseye.baker@gmail.com>
2019-04-02 07:18:16 -0700

    adds video files to repo; begins building fullscreen video background

--- front-end/src/components/Construction.js
+++ front-end/src/components/Construction.js
@@ -1,30 +1,19 @@
 //== Dependencies ==//
-import React, { Component } from 'react'
-import Parallax from 'parallax-js' 
+import React, { Component } from "react";
+import Parallax from "parallax-js";
 
 //== Components ==//
+import Fire from "../assets/video/fire-loop.mp4";
 
 class Construction extends Component {
-  componentDidMount() {
-    this.parallax = new Parallax(this.scene)
-  }
-  componentWillUnmount() {
-    this.parallax.disable()
-  }
   render() {
-    render (
-      <ul ref={el => this.scene = el}>
-        <li className="layer" dataDepth="0.00">
-          <img src="layer1.png"/>
-        </li>
-        <li className="layer" dataDepth="0.50">
-          <img src="layer2.png"/>
-        </li>
-        <li className="layer" dataDepth="1.00">
-          <img src="layer3.png"/>
-        </li>
-      </ul>
-    )
+    return (
+      <div className="Construction">
+        <video autoplay muted loop id="myVideo">
+          <source src={Fire} type="video/mp4" />
+        </video>
+      </div>
+    );
   }
 }
 
