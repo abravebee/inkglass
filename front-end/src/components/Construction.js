897f0b818040f93f45c00e6ef8fa76f3d45f6f8a
Casey Bee <caseye.baker@gmail.com>
2019-04-02 08:25:14 -0700

    added icons to repo; got background video working without scrollbars

--- front-end/src/components/Construction.js
+++ front-end/src/components/Construction.js
@@ -1,16 +1,32 @@
 //== Dependencies ==//
 import React, { Component } from "react";
-import Parallax from "parallax-js";
 
 //== Components ==//
-import Fire from "../assets/video/fire-loop.mp4";
+import Fire from "../assets/video/fire-loop.mov";
+import Air from "../assets/video/air.mp4"
+import Air2 from "../assets/video/air2.mp4"
+import Earth from "../assets/video/earth-loop.mov"
+import Water from "../assets/video/water-loop.mov";
+
+//== Style ==//
+import "./Construction.css"
 
 class Construction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      earth: false,
+      water: false,
+      air: false,
+      fire: false
+    }
+  }
+ 
   render() {
     return (
       <div className="Construction">
-        <video autoplay muted loop id="myVideo">
-          <source src={Fire} type="video/mp4" />
+        <video autoPlay muted loop id="myVideo">
+          <source src={Air2}/>
         </video>
 
         <div className="content">
