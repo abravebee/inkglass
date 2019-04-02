b6c2f330f06977eb404c48a40dd988f3c311c1a4
Casey Bee <caseye.baker@gmail.com>
2019-04-02 10:19:44 -0700

    adds icons for other theming

--- front-end/src/components/Construction.js
+++ front-end/src/components/Construction.js
@@ -7,6 +7,10 @@ import Air from "../assets/video/air.mp4"
 import Air2 from "../assets/video/air2.mp4"
 import Earth from "../assets/video/earth-loop.mov"
 import Water from "../assets/video/water-loop.mov";
+import FireIcon from "../assets/svg-icons/fire-2.svg"
+import AirIcon from "../assets/svg-icons/wind.svg"
+import EarthIcon from "../assets/svg-icons/leaf-2.svg"
+import WaterIcon from "../assets/svg-icons/water-2.svg"
 
 //== Style ==//
 import "./Construction.css"
@@ -26,12 +30,20 @@ class Construction extends Component {
     return (
       <div className="Construction">
         <video autoPlay muted loop id="myVideo">
-          <source src={Air2}/>
+          <source src={Earth}/>
         </video>
 
         <div className="content">
-          <h1>Inkglass</h1>
-          <p>Coming Soon</p>
+        <div className="text glass">
+          <h1 className="header">Inkglass</h1>
+          <p className="paragraph">Coming Soon</p>
+          </div>
+          <div className="elements">
+            <img className="element-icon fire" src={FireIcon} />
+            <img className="element-icon" src={AirIcon} />
+            <img className="element-icon" src={WaterIcon} />
+            <img className="element-icon" src={EarthIcon} />
+          </div>
         </div>
       </div>
     );
