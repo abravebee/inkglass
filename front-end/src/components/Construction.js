61e71455b3bc0886f18988481cca1e46e3f588eb
Casey Bee <caseye.baker@gmail.com>
2019-04-02 14:47:11 -0700

    can now switch between elements

--- front-end/src/components/Construction.js
+++ front-end/src/components/Construction.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 
 //== Components ==//
 import Fire from "../assets/video/fire-loop.mov";
-import Air from "../assets/video/air.mp4"
-import Air2 from "../assets/video/air2.mp4"
+import Air from "../assets/video/air2.mp4"
 import Earth from "../assets/video/earth-loop.mov"
 import Water from "../assets/video/water-loop.mov";
 import FireIcon from "../assets/svg-icons/fire-2.svg"
@@ -19,34 +18,44 @@ class Construction extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      earth: false,
-      water: false,
+      fire: false,
       air: false,
-      fire: false
+      water: true,
+      earth: false
     }
   }
 
   fire = () => {
     console.log("FIRE CLICK")
+    this.setState({fire: true, air: false, water: false, earth: false}, () => {console.log("fire state", this.state)})
   }
 
   air = () => {
     console.log("AIR CLICK")
+    this.setState({fire: false, air: true, water: false, earth: false}, () => {console.log("air state", this.state)})
   }
 
   water = () => {
     console.log("WATER CLICK")
+    this.setState({fire: false, air: false, water: true, earth: false}, () => {console.log("water state", this.state)})
   }
   
   earth = () => {
     console.log("EARTH CLICK")
+    this.setState({fire: false, air: false, water: false, earth: true}, () => {console.log("earth state", this.state)})
   }
  
   render() {
+    console.log("render state", this.state)
+    let backgroundSrc = (this.state.fire ? Fire 
+      : this.state.air ? Air
+      : this.state.water ? Water 
+      : Earth)
+      console.log("bg", backgroundSrc)
     return (
       <div className="Construction">
-        <video autoPlay muted loop id="myVideo">
-          <source src={Earth}/>
+        <video autoPlay muted loop id="myVideo" src={backgroundSrc}>
+          {/* <source /> */}
         </video>
         <div className="glass-container">
         <div className="glass">
@@ -59,15 +68,24 @@ class Construction extends Component {
           <p className="paragraph">Coming Soon</p>
           </div>
           <div className="elements">
-            <img className="element-icon fire" src={FireIcon} onClick={() => {this.fire()}} />
-            <img className="element-icon air" src={AirIcon}  onClick={() => {this.air()}} />
-            <img className="element-icon water" src={WaterIcon}  onClick={() => {this.water()}} />
-            <img className="element-icon earth active" src={EarthIcon}  onClick={() => {this.earth()}} />
+            <img 
+              className={`element-icon fire ${this.state.fire?'active':''}`}
+              src={FireIcon} 
+              onClick={() => {this.fire()}} />
+            <img 
+              className={`element-icon air ${this.state.air?'active':''}`}
+              src={AirIcon}  
+              onClick={() => {this.air()}} />
+            <img 
+              className={`element-icon water ${this.state.water?'active':''}`} 
+              src={WaterIcon}  
+              onClick={() => {this.water()}} />
+            <img 
+              className={`element-icon earth ${this.state.earth?'active':''}`} 
+              src={EarthIcon}  
+              onClick={() => {this.earth()}} />
           </div>
         </div>
-
-    
-
       </div>
     );
   }
